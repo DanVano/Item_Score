a6c63924e1977d51261948ecd4b66f9c9f07e139
Daniel V <98115971+DanVano@users.noreply.github.com>
2024-10-15 13:54:12 -0700

    Add files via upload

---
+++ ItemScore.lua
@@ -0,0 +1,167 @@
+scoredItemTypes = { 
+  INVTYPE_2HWEAPON, INVTYPE_CHEST, INVTYPE_CLOAK,
+  INVTYPE_FEET, INVTYPE_FINGER, INVTYPE_HAND, INVTYPE_HEAD, INVTYPE_HOLDABLE,
+  INVTYPE_LEGS, INVTYPE_NECK, INVTYPE_RANGED, INVTYPE_RELIC, INVTYPE_ROBE, INVTYPE_SHIELD,
+  INVTYPE_SHOULDER, INVTYPE_TRINKET, INVTYPE_WAIST, INVTYPE_WEAPON,
+  INVTYPE_WEAPONMAINHAND, INVTYPE_WEAPONOFFHAND, INVTYPE_WRIST,
+  -- deDE
+  "Schusswaffe", "Zauberstab", "Armbrust",
+  -- enGB
+  "Gun", "Wand", "Crossbow" 
+}
+
+
+function istable(t)
+  return type(t) == 'table'
+end
+
+-- Round up to the nearest whole number
+function VRBRound(num, places)
+  local mult = 10^(places or 0)
+  return math.ceil(num * mult) / mult
+end
+
+
+function VRBCheckItemType(slot)
+  for id, scoredSlot in pairs(scoredItemTypes) do
+    if slot == scoredSlot then
+      return true
+    end
+  end
+  return nil
+end
+
+
+function VRBGetValidRatings()
+  local ratings
+  local class = UnitClass("player")
+
+  ratings = VRB_LABELS[class]
+  if ratings then
+    return ratings
+  end
+  return {}
+
+end
+
+
+function VRBCalculateRating(weightTable, bonuses)
+
+  local baseScore = 0
+  local bonus, i;
+  local weightTypes = VRB_WEIGHTS[weightTable]
+  local currentBonus = 0
+
+  for t,w in pairs(weightTypes) do
+  
+    if (BonusScanner.bonuses[t]) then
+      currentBonus = BonusScanner.bonuses[t]
+    end
+
+    if(bonuses[t]) then
+      -- Now check if the weight is a compound structure; has a threshold
+      if istable(w) then
+        threshold = w[1]
+        beforeWeight = w[2]
+        afterWeight = w[3]
+        if tonumber(currentBonus) < tonumber(threshold) then
+          baseScore = baseScore + ( bonuses[t] * beforeWeight )
+        else
+          baseScore = baseScore + ( bonuses[t] * afterWeight )
+        end
+      else
+        baseScore = baseScore + ( bonuses[t] * w )
+      end
+    end
+
+  end
+
+  return VRBRound(baseScore, 0)  -- Rounded to nearest whole number
+
+end
+
+
+VRBItemScoreTooltip = CreateFrame("Frame", "VRBItemScoreTooltip", GameTooltip)
+
+VRBItemScoreTooltip:SetScript("OnShow", function(self)
+    local itemLevel = nil
+    local itemRarity = nil
+    local itemSlot = nil
+    local bonuses = nil
+    local tmpTxt, line
+    local lines = GameTooltip:NumLines()
+    local hasScoreToShow = false
+
+    BonusScanner.temp.sets = {}
+    BonusScanner.temp.set = ""
+    BonusScanner.temp.bonuses = {}
+    BonusScanner.temp.slot = ""
+
+    -- Check if the player is a Druid
+    local className, classFileName = UnitClass("player")
+    if classFileName ~= "DRUID" then
+        return  -- Exit if the player is not a Druid
+    end
+
+    -- Get the player's class color
+    local color = RAID_CLASS_COLORS[classFileName]
+
+    -- Process tooltip lines and scan item stats
+    local lbl = getglobal("GameTooltipTextLeft1")
+    if lbl then
+        for i = 2, lines, 1 do
+            tmpText = getglobal("GameTooltipTextLeft" .. i)
+            val = nil
+            if (tmpText:GetText()) then
+                line = tmpText:GetText()
+                BonusScanner:ScanLine(line)
+            end
+        end
+
+        bonuses = BonusScanner.temp.bonuses
+
+        if bonuses then
+            local ratings = VRBGetValidRatings()
+
+            -- Add logic to handle "Bear" and "BearDps" combined display
+            local bearScore = 0
+            local bearDpsScore = 0
+            local totalBearScore = 0
+
+            for i, r in ipairs(ratings) do
+                if r == "Bear" then
+                    -- Calculate Bear score
+                    bearScore = VRBCalculateRating("Bear", bonuses)
+                    -- Calculate BearDps score
+                    bearDpsScore = VRBCalculateRating("BearDps", bonuses)
+                    -- Combine Bear and BearDps
+                    totalBearScore = bearScore + bearDpsScore
+
+                    -- Display Bear as "Bear:Bear // (Bear + BearDps)" only if relevant
+                    if bearScore > 0 or bearDpsScore > 0 then
+                        GameTooltip:AddDoubleLine("Bear:", bearScore .. " // " .. totalBearScore, 
+                        color.r, color.g, color.b,  -- First part (label color)
+                        color.r, color.g, color.b)  -- Second part (value color)
+                        hasScoreToShow = true
+                    end
+                elseif r ~= "BearDps" then
+                    -- Display other labels as normal (e.g., Tree, Cat)
+                    local vrbscore = VRBCalculateRating(r, bonuses)
+                    if vrbscore > 0 then
+                        local normalizedLabel = string.gsub(r, className, "")
+                        GameTooltip:AddDoubleLine(normalizedLabel .. ":", vrbscore, 
+                        color.r, color.g, color.b,  -- First part (label color)
+                        color.r, color.g, color.b)  -- Second part (value color)
+                        hasScoreToShow = true
+                    end
+                end
+            end
+
+            if hasScoreToShow then
+                GameTooltip:Show()
+            end
+        end
+    end
+end)
+
+SLASH_VRBSCORE1, SLASH_VRBSCORE2 = '/vrb';
---
+++ ItemScoreWeights.lua
@@ -0,0 +1,235 @@
+﻿-- From BonusScanner
+-- types = {
+--   "STR",      -- strength 
+--   "AGI",      -- agility
+--   "STA",      -- stamina
+--   "INT",      -- intellect
+--   "SPI",      -- spirit
+--   "ARMOR",    -- reinforced armor (not base armor)
+
+--   "ARCANERES",  -- arcane resistance
+--   "FIRERES",    -- fire resistance
+--   "NATURERES",  -- nature resistance  
+--   "FROSTRES",   -- frost resistance
+--   "SHADOWRES",  -- shadow resistance
+
+--   "FISHING",    -- fishing skill
+--   "MINING",   -- mining skill
+--   "HERBALISM",  -- herbalism skill
+--   "SKINNING",   -- skinning skill
+--   "DEFENSE",    -- defense skill
+
+--   "BLOCK",      -- chance to block
+--   "DODGE",    -- chance to dodge
+--   "PARRY",    -- chance to parry
+--   "ATTACKPOWER",  -- attack power
+--   "ATTACKPOWERUNDEAD", -- attack power against undead
+  
+--   "CRIT",     -- chance to get a critical strike
+--   "RANGEDATTACKPOWER", -- ranged attack power
+--   "RANGEDCRIT", -- chance to get a crit with ranged weapons
+--   "TOHIT",    -- chance to hit
+
+--   "DMG",      -- spell damage
+--   "DMGUNDEAD",  -- spell damage against undead
+  
+--   "ARCANEDMG",  -- arcane spell damage
+--   "FIREDMG",    -- fire spell damage
+--   "FROSTDMG",   -- frost spell damage
+--   "HOLYDMG",    -- holy spell damage
+--   "NATUREDMG",  -- nature spell damage
+--   "SHADOWDMG",  -- shadow spell damage
+--   "SPELLCRIT",  -- chance to crit with spells
+--   "HEAL",     -- healing 
+--   "HOLYCRIT",   -- chance to crit with holy spells
+--   "SPELLTOHIT",   -- Chance to Hit with spells
+
+--   "SPELLPEN",   -- amount of spell resist reduction
+
+--   "HEALTHREG",  -- health regeneration per 5 sec.
+--   "MANAREG",    -- mana regeneration per 5 sec.
+--   "HEALTH",   -- health points
+--   "MANA",     -- mana points
+-- };
+
+VRB_LABELS = {
+    ["Druid"]   = { "Tree", "Cat", "Bear", "BearDps" },
+    ["Shaman"]  = { "ShamanHEP" },
+    ["Warrior"] = { "WarriorProtEH", "WarriorProtAvoidance", "WarriorArmsDPS", "WarriorFuryDPS" },
+    ["Paladin"] = { "PaladinProtEH", "PaladinRetDPS", "PaladinHEP" },
+    ["Priest"]  = { "PriestHEP_2M", "PriestHEP_15M", "PriestShadowDPS_2M" }
+--    ["Priest"]  = { "Priest2MinHEP", "Priest15MinHEP", "Priest2MinHEPT2Bonus", "Priest15MinHEPT2Bonus" }
+}
+
+
+VRB_WEIGHTS = {
+
+  ["PaladinRetDPS"] = {
+    ["STR"]         = 0.1428571428571429,
+    ["AGI"]         = 0.1020,
+    ["CRIT"]        = 2.04,
+    ["TOHIT"]       = { 5.6, 100, 0 },
+    ["ATTACKPOWER"] = 0.0714285714285714
+  }, 
+
+  ["PaladinHEP"] = {
+    ["INT"]       = 1.00,
+    ["MANAREG"]   = 1.91,
+    ["HEAL"]      = 1.00,
+    ["SPELLCRIT"] = 20.34,
+    ["SPI"]       = 0.00
+  },  
+
+  ["PaladinProtEH"] = {
+    ["ARMOR"]   = 0.51416,
+    ["STA"]     = 10.14596,
+    ["AGI"]     = 1.02832,
+    ["STR"]     = 0.05,
+    ["DODGE"]   = 0,
+    ["PARRY"]   = 0,
+    ["DEFENSE"] = 0.08,
+    ["BLOCK"]   = 0.04
+  },  
+
+  ["Tree"] = {
+    ["INT"]       = 0.30,
+    ["MANAREG"]   = 5.00,
+    ["HEAL"]      = 1.00,
+    ["SPELLCRIT"] = 10,
+    ["SPI"]       = 0.47
+  },
+
+  ["HEPNAXX"] = {
+    ["INT"]       = 0.30,
+    ["MANAREG"]   = 3.00,
+    ["HEAL"]      = 1.20,
+    ["SPELLCRIT"] = 12.00,
+    ["SPI"]       = 0.23
+  },
+
+
+  ["Cat"] = {
+    ["STR"]         = 2.2,
+    ["AGI"]         = 2.2,
+    ["CRIT"]        = 24,
+    ["TOHIT"]       = { 8, 15, 0 }, 
+    ["ATTACKPOWER"] = 1
+  },
+
+  ["Bear"] = {
+    ["AGI"]      = 2.5,
+    ["ARMOR"]    = 0.81,
+    ["DEFENSE"]  = 2,
+    ["DODGE"]    = 20,
+    ["STA"]      = 6.242,
+    ["HEALTHREG"] = 2,
+  },
+
+  ["BearDps"] = {
+    ["STR"]         = 2,
+    ["AGI"]         = 1.5,
+    ["CRIT"]        = 29,
+    ["TOHIT"]       = { 8, 30, 0 }, 
+    ["ATTACKPOWER"] = 1,
+    ["DODGE"]       =-20
+  },
+
+  ["ShamanHEP"] = {
+    ["INT"]       = 0.20,
+    ["MANAREG"]   = 1.00,
+    ["HEAL"]      = 0.14,
+    ["SPELLCRIT"] = 0.00,
+    ["SPI"]       = 0.00
+  },
+
+  ["PriestHEP_2M"] = {
+    ["INT"]       = 2.322635135,
+    ["MANAREG"]   = 3.5,
+    ["HEAL"]      = 1,
+    ["SPELLCRIT"] = 8,
+    ["SPI"]       = 0.8275
+  },
+
+  ["PriestHEP_2M_T2"] = {
+    ["INT"]       = 2.322635135,
+    ["MANAREG"]   = 3.5,
+    ["HEAL"]      = 1,
+    ["SPELLCRIT"] = 8,
+    ["SPI"]       = 1.1775
+  },
+  
+  ["PriestHEP_15M"] = {
+    ["INT"]       = 0.4351,
+    ["MANAREG"]   = 3.5,
+    ["HEAL"]      = 1,
+    ["SPELLCRIT"] = 8,
+    ["SPI"]       = 0.8275
+  },  
+
+  ["PriestHEP_15M_T2"] = {
+    ["INT"]       = 0.4351,
+    ["MANAREG"]   = 3.5,
+    ["HEAL"]      = 1,
+    ["SPELLCRIT"] = 8,
+    ["SPI"]       = 1.1775
+  },  
+
+
+  ["PriestShadowDPS_2M"] = {
+    ["DMG"]        = 0.387742,
+    ["SHADOWDMG"]  = 0.387742,
+    ["INT"]        = 1.093871,
+    ["MANAREG"]    = 0.077742,
+    ["SPI"]        = 0.006452,
+    ["SPELLCRIT"]  = 0.018231,
+    ["SPELLTOHIT"] = { 8, 100, 0 }, 
+  },
+
+  ["PriestShadowDPS_15M"] = {
+    ["DMG"]        = 0.39,
+    ["SHADOWDMG"]  = 0.39,
+    ["INT"]        = 0.546774,
+    ["MANAREG"]    = 0.077742,
+    ["SPI"]        = 0.012903,
+    ["SPELLCRIT"]  = 0.009113,
+    ["SPELLTOHIT"] = { 8, 100, 0 }, 
+  },
+
+  ["WarriorProtEH"] = {
+    ["ARMOR"]   = 0.51416,
+    ["STA"]     = 10.14596,
+    ["AGI"]     = 1.02832,
+    ["STR"]     = 0.05,
+    ["DODGE"]   = 0,
+    ["PARRY"]   = 0,
+    ["DEFENSE"] = 0.08,
+    ["BLOCK"]   = 0.04
+  },
+
+  ["WarriorProtAvoidance"] = {
+    ["ARMOR"]   = 0,
+    ["STA"]     = 0,
+    ["AGI"]     = 0.05,
+    ["STR"]     = 0,
+    ["DODGE"]   = 1,
+    ["PARRY"]   = 0.04,
+    ["DEFENSE"] = 0.08
+  },
+
+  ["WarriorArmsDPS"] = {
+    ["STR"]         = 0.1428571428571429,
+    ["AGI"]         = 0.1020,
+    ["CRIT"]        = 2.04,
+    ["TOHIT"]       = { 8.6, 2.04, 0 },
+    ["ATTACKPOWER"] = 0.0714285714285714
+  },
+
+  ["WarriorFuryDPS"] = {
+    ["STR"]         = 0.1428571428571429,
+    ["AGI"]         = 0.1020,
+    ["CRIT"]        = 2.04,
+    ["TOHIT"]       = { 14, 2.04, 0 },
+    ["ATTACKPOWER"] = 0.0714285714285714    
+  }
+
+}
