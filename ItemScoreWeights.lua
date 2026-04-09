759f3a9362df6f144c0976831437d07c8caffab2
DanVano <dvanovcan@gmail.com>
2026-04-08 23:38:49 -0700

    Bug fixed API calls to BonusScanner

--- ItemScoreWeights.lua
+++ ItemScoreWeights.lua
@@ -1,6 +1,6 @@
-﻿-- From BonusScanner
+-- From BonusScanner
 -- types = {
---   "STR",      -- strength 
+--   "STR",      -- strength
 --   "AGI",      -- agility
 --   "STA",      -- stamina
 --   "INT",      -- intellect
@@ -9,7 +9,7 @@
 
 --   "ARCANERES",  -- arcane resistance
 --   "FIRERES",    -- fire resistance
---   "NATURERES",  -- nature resistance  
+--   "NATURERES",  -- nature resistance
 --   "FROSTRES",   -- frost resistance
 --   "SHADOWRES",  -- shadow resistance
 
@@ -20,11 +20,13 @@
 --   "DEFENSE",    -- defense skill
 
 --   "BLOCK",      -- chance to block
+--   "BLOCKVALUE", -- increased block value
 --   "DODGE",    -- chance to dodge
 --   "PARRY",    -- chance to parry
 --   "ATTACKPOWER",  -- attack power
 --   "ATTACKPOWERUNDEAD", -- attack power against undead
-  
+--   "ATTACKPOWERFERAL",  -- attack power in feral form (Cat, Bear, Dire Bear)
+
 --   "CRIT",     -- chance to get a critical strike
 --   "RANGEDATTACKPOWER", -- ranged attack power
 --   "RANGEDCRIT", -- chance to get a crit with ranged weapons
@@ -32,7 +34,7 @@
 
 --   "DMG",      -- spell damage
 --   "DMGUNDEAD",  -- spell damage against undead
-  
+
 --   "ARCANEDMG",  -- arcane spell damage
 --   "FIREDMG",    -- fire spell damage
 --   "FROSTDMG",   -- frost spell damage
@@ -40,7 +42,7 @@
 --   "NATUREDMG",  -- nature spell damage
 --   "SHADOWDMG",  -- shadow spell damage
 --   "SPELLCRIT",  -- chance to crit with spells
---   "HEAL",     -- healing 
+--   "HEAL",     -- healing
 --   "HOLYCRIT",   -- chance to crit with holy spells
 --   "SPELLTOHIT",   -- Chance to Hit with spells
 
@@ -57,8 +59,11 @@ VRB_LABELS = {
     ["Shaman"]  = { "ShamanHEP" },
     ["Warrior"] = { "WarriorProtEH", "WarriorProtAvoidance", "WarriorArmsDPS", "WarriorFuryDPS" },
     ["Paladin"] = { "PaladinProtEH", "PaladinRetDPS", "PaladinHEP" },
-    ["Priest"]  = { "PriestHEP_2M", "PriestHEP_15M", "PriestShadowDPS_2M" }
---    ["Priest"]  = { "Priest2MinHEP", "Priest15MinHEP", "Priest2MinHEPT2Bonus", "Priest15MinHEPT2Bonus" }
+    ["Priest"]  = { "PriestHEP_2M", "PriestHEP_15M", "PriestShadowDPS_2M" },
+    ["Hunter"]  = { "HunterDamage" },
+    ["Rogue"]   = { "RogueDamage" },
+    ["Mage"]    = { "MageDamage" },
+    ["Warlock"] = { "WarlockDamage" },
 }
 
 
@@ -70,7 +75,7 @@ VRB_WEIGHTS = {
     ["CRIT"]        = 2.04,
     ["TOHIT"]       = { 5.6, 100, 0 },
     ["ATTACKPOWER"] = 0.0714285714285714
-  }, 
+  },
 
   ["PaladinHEP"] = {
     ["INT"]       = 1.00,
@@ -78,7 +83,7 @@ VRB_WEIGHTS = {
     ["HEAL"]      = 1.00,
     ["SPELLCRIT"] = 20.34,
     ["SPI"]       = 0.00
-  },  
+  },
 
   ["PaladinProtEH"] = {
     ["ARMOR"]   = 0.51416,
@@ -89,7 +94,7 @@ VRB_WEIGHTS = {
     ["PARRY"]   = 0,
     ["DEFENSE"] = 0.08,
     ["BLOCK"]   = 0.04
-  },  
+  },
 
   ["Tree"] = {
     ["INT"]       = 0.30,
@@ -99,39 +104,32 @@ VRB_WEIGHTS = {
     ["SPI"]       = 0.47
   },
 
-  ["HEPNAXX"] = {
-    ["INT"]       = 0.30,
-    ["MANAREG"]   = 3.00,
-    ["HEAL"]      = 1.20,
-    ["SPELLCRIT"] = 12.00,
-    ["SPI"]       = 0.23
-  },
-
-
   ["Cat"] = {
-    ["STR"]         = 2.2,
-    ["AGI"]         = 2.2,
-    ["CRIT"]        = 24,
-    ["TOHIT"]       = { 8, 15, 0 }, 
-    ["ATTACKPOWER"] = 1
+    ["STR"]              = 2.2,
+    ["AGI"]              = 2.2,
+    ["CRIT"]             = 24,
+    ["TOHIT"]            = { 8, 15, 0 },
+    ["ATTACKPOWER"]      = 1,
+    ["ATTACKPOWERFERAL"] = 1,
   },
 
   ["Bear"] = {
-    ["AGI"]      = 2.5,
-    ["ARMOR"]    = 0.81,
-    ["DEFENSE"]  = 2,
-    ["DODGE"]    = 20,
-    ["STA"]      = 6.242,
+    ["AGI"]       = 2.5,
+    ["ARMOR"]     = 0.81,
+    ["DEFENSE"]   = 2,
+    ["DODGE"]     = 20,
+    ["STA"]       = 6.242,
     ["HEALTHREG"] = 2,
   },
 
   ["BearDps"] = {
-    ["STR"]         = 2,
-    ["AGI"]         = 1.5,
-    ["CRIT"]        = 29,
-    ["TOHIT"]       = { 8, 30, 0 }, 
-    ["ATTACKPOWER"] = 1,
-    ["DODGE"]       =-20
+    ["STR"]              = 2,
+    ["AGI"]              = 1.5,
+    ["CRIT"]             = 29,
+    ["TOHIT"]            = { 8, 30, 0 },
+    ["ATTACKPOWER"]      = 1,
+    ["ATTACKPOWERFERAL"] = 1,
+    ["DODGE"]            = -20
   },
 
   ["ShamanHEP"] = {
@@ -150,30 +148,13 @@ VRB_WEIGHTS = {
     ["SPI"]       = 0.8275
   },
 
-  ["PriestHEP_2M_T2"] = {
-    ["INT"]       = 2.322635135,
-    ["MANAREG"]   = 3.5,
-    ["HEAL"]      = 1,
-    ["SPELLCRIT"] = 8,
-    ["SPI"]       = 1.1775
-  },
-  
   ["PriestHEP_15M"] = {
     ["INT"]       = 0.4351,
     ["MANAREG"]   = 3.5,
     ["HEAL"]      = 1,
     ["SPELLCRIT"] = 8,
     ["SPI"]       = 0.8275
-  },  
-
-  ["PriestHEP_15M_T2"] = {
-    ["INT"]       = 0.4351,
-    ["MANAREG"]   = 3.5,
-    ["HEAL"]      = 1,
-    ["SPELLCRIT"] = 8,
-    ["SPI"]       = 1.1775
-  },  
-
+  },
 
   ["PriestShadowDPS_2M"] = {
     ["DMG"]        = 0.387742,
@@ -182,17 +163,7 @@ VRB_WEIGHTS = {
     ["MANAREG"]    = 0.077742,
     ["SPI"]        = 0.006452,
     ["SPELLCRIT"]  = 0.018231,
-    ["SPELLTOHIT"] = { 8, 100, 0 }, 
-  },
-
-  ["PriestShadowDPS_15M"] = {
-    ["DMG"]        = 0.39,
-    ["SHADOWDMG"]  = 0.39,
-    ["INT"]        = 0.546774,
-    ["MANAREG"]    = 0.077742,
-    ["SPI"]        = 0.012903,
-    ["SPELLCRIT"]  = 0.009113,
-    ["SPELLTOHIT"] = { 8, 100, 0 }, 
+    ["SPELLTOHIT"] = { 8, 100, 0 },
   },
 
   ["WarriorProtEH"] = {
@@ -229,7 +200,46 @@ VRB_WEIGHTS = {
     ["AGI"]         = 0.1020,
     ["CRIT"]        = 2.04,
     ["TOHIT"]       = { 14, 2.04, 0 },
-    ["ATTACKPOWER"] = 0.0714285714285714    
-  }
+    ["ATTACKPOWER"] = 0.0714285714285714
+  },
+
+  -- Weights imported from original ItemScore addon
+  ["HunterDamage"] = {
+    ["AGI"]               = 1.2,
+    ["RANGEDATTACKPOWER"] = 0.5,
+    ["ATTACKPOWER"]       = 0.5,
+    ["INT"]               = 1.5,
+    ["MANA"]              = 0.2,
+    ["CRIT"]              = 10,
+    ["RANGEDCRIT"]        = 10,
+    ["TOHIT"]             = 10,
+  },
+
+  ["RogueDamage"] = {
+    ["STR"]         = 0.9,
+    ["AGI"]         = 1,
+    ["ATTACKPOWER"] = 0.9,
+    ["CRIT"]        = 10,
+    ["TOHIT"]       = 10,
+  },
+
+  ["MageDamage"] = {
+    ["INT"]        = 1.2,
+    ["MANA"]       = 0.07,
+    ["DMG"]        = 1,
+    ["FIREDMG"]    = 0.8,
+    ["FROSTDMG"]   = 0.8,
+    ["SPELLCRIT"]  = 10,
+    ["SPELLTOHIT"] = 15,
+  },
+
+  ["WarlockDamage"] = {
+    ["STA"]        = 1,
+    ["INT"]        = 0.5,
+    ["DMG"]        = 1,
+    ["SHADOWDMG"]  = 0.8,
+    ["SPELLCRIT"]  = 15,
+    ["SPELLTOHIT"] = 10,
+  },
 
 }
